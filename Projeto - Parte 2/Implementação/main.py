521ff27e83597362349a50472d16c45e4143ec6d
ThiagoAF25 <88636387+ThiagoAF25@users.noreply.github.com>
2023-11-25 11:56:46 -0300

    Membros do grupo

--- Projeto - Parte 2/Implementação/main.py
+++ Projeto - Parte 2/Implementação/main.py
@@ -1,3 +1,11 @@
+'''
+Membros do grupo:
+Leonardo Pinheiro de Souza      -   32127391
+Lucas Paulo da Rocha            -   32196628
+Luiz Octavio Tassinari Saraiva  -   32030411
+Thiago Aidar Figueiredo         -   32144547
+'''
+
 from utils import menu
 
 menu()
