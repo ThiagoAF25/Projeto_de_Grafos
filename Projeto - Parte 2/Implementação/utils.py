6a5a9dc61976dc9e63d0323411a9111515ac428d
Luiz Saraiva <83554105+Lukxus@users.noreply.github.com>
2023-11-26 16:12:11 -0300

    Update utils.py

--- Projeto - Parte 2/Implementação/utils.py
+++ Projeto - Parte 2/Implementação/utils.py
@@ -1,3 +1,11 @@
+'''
+Membros do grupo:
+Leonardo Pinheiro de Souza      -   32127391
+Lucas Paulo da Rocha            -   32196628
+Luiz Octavio Tassinari Saraiva  -   32030411
+Thiago Aidar Figueiredo         -   32144547
+'''
+
 from grafoNDMatriz import GrafoND
 import pandas as pd
 import numpy as np
